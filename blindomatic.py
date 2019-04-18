270b53ec29303c8e5165df87d8f0e54271830c35
Jeff Choate <choate.ja@gmail.com>
2019-04-18 13:45:15 -0400

    Added hysteresis for control over light sensor

--- blindomatic.py
+++ blindomatic.py
@@ -9,11 +9,46 @@ import pdb
 
 # Number of seconds between each reading of photo sensor
 READ_SENSOR_SECONDS = 3
-# Adding historesis - must get this many consistent readings
+# Number of seconds to gather data to make a decision
+OBSERVE_PERIOD = 300
+# Adding hysteresis - must get this many consistent readings
 # in a row before an action is taken
 SMOOTHING_LEVEL = 5
 
 PHOTO_INPUT = 4
+def evaluateLightLevel( period=OBSERVE_PERIOD ):
+  numReadings = period/READ_SENSOR_SECONDS
+  lightCount = darkCount = 0
+  while numReadings:
+    lightData=GPIO.input(PHOTO_INPUT)
+    if lightData:
+      darkCount += 1
+    else:
+      lightCount += 1
+    numReadings -= 1
+    sleep(READ_SENSOR_SECONDS)
+
+  #Take our counted data and determine what state we should be in
+  if darkCount > 0:
+    ratio = lightCount / float( darkCount)
+  else:
+    # if darkCount is 0, then we absolutely need to close the blinds
+    ctrl.logMsg(ctrl.logFileName, "Only light reading - no ratio available")
+    return ctrl.closedState
+
+  # Use the ratio for hysteresis
+  # 0 - 0.5 ratio means darkCount is decidedly more, return open state
+  # Any value greater than 2 means that numerator (lightCount) is decidedly
+  # more so return closed state.
+  # Values between 0.5 to 1.5 are too close to call, return unknown state
+  logMessage = ( "light: %d dark: %d, Ratio was %f" %
+                 (lightCount, darkCount, ratio))
+  Ctrl.Logmsg(ctrl.logFileName, logMessage)
+  if ratio <= 0.5:
+    return ctrl.openState
+  if ratio >= 1.5:
+    return ctrl.closedState
+  return ctrl.unknownState
 
 def main(argv):
   desiredAction=''
@@ -62,33 +97,41 @@ def main(argv):
 
     try:
       while True:
-        lightData=GPIO.input(PHOTO_INPUT)
-        # Prevent overflow
-        lightCount = 1 if (lightCount == sys.maxint) else lightCount 
-        darkCount = 1 if (darkCount == sys.maxint) else darkCount 
-        if lightData:
-          if darkCount > 0:
-            lightCount = darkCount = 0
-          lightCount += 1
-          if lightCount >= SMOOTHING_LEVEL:
-            if ctrl.openBlinds():
-              # Wait for 5 minutes here before moving on
-              # after a state change
-              ctrl.logMsg( ctrl.logFileName, "Sleeping for 5 mins." )
-              sleep( 300 )
-              ctrl.logMsg( ctrl.logFileName, "Resuming operation." )
-        else:
-          if lightCount > 0:
-            lightCount = darkCount = 0
-          darkCount += 1
-          if darkCount >= SMOOTHING_LEVEL:
-            if ctrl.closeBlinds():
-              # Wait for 5 minutes here before moving on
-              # after a state change
-              ctrl.logMsg( ctrl.logFileName, "Sleeping for 5 mins." )
-              sleep( 300 )
-              ctrl.logMsg( ctrl.logFileName, "Resuming operation." )
-        sleep( READ_SENSOR_SECONDS )
+        decision = evaluateLightLevel()
+        if decision == ctrl.openState:
+          ctrl.openBlinds()
+        elif decision == ctrl.closedState:
+          ctrl.closeBlinds()
+        elif decision == ctrl.unknownState:
+          ctrl.logMsg( ctrl.logFileName, "Unknown blind state. Doing nothing" )
+
+        # lightData=GPIO.input(PHOTO_INPUT)
+        # # Prevent overflow
+        # lightCount = 1 if (lightCount == sys.maxint) else lightCount 
+        # darkCount = 1 if (darkCount == sys.maxint) else darkCount 
+        # if lightData:
+        #   if darkCount > 0:
+        #     lightCount = darkCount = 0
+        #   lightCount += 1
+        #   if lightCount >= SMOOTHING_LEVEL:
+        #     if ctrl.openBlinds():
+        #       # Wait for 5 minutes here before moving on
+        #       # after a state change
+        #       ctrl.logMsg( ctrl.logFileName, "Sleeping for 5 mins." )
+        #       sleep( 300 )
+        #       ctrl.logMsg( ctrl.logFileName, "Resuming operation." )
+        # else:
+        #   if lightCount > 0:
+        #     lightCount = darkCount = 0
+        #   darkCount += 1
+        #   if darkCount >= SMOOTHING_LEVEL:
+        #     if ctrl.closeBlinds():
+        #       # Wait for 5 minutes here before moving on
+        #       # after a state change
+        #       ctrl.logMsg( ctrl.logFileName, "Sleeping for 5 mins." )
+        #       sleep( 300 )
+        #       ctrl.logMsg( ctrl.logFileName, "Resuming operation." )
+        # sleep( READ_SENSOR_SECONDS )
     except:
       ctrl.logMsg( ctrl.logFileName, "Exiting system" )
       GPIO.cleanup() # ensures a clean exit
